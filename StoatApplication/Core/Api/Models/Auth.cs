9fe3495a2610ef1768e83937b2eae63c12e4ca9b
Bloom Perez <springdotgay@tuta.io>
2025-12-18 17:50:58 -0400

    docs: Add documentation for Auth
    Signed-off-by: Bloom Perez <springdotgay@tuta.io>

--- StoatApplication/Core/Api/Models/Auth.cs
+++ StoatApplication/Core/Api/Models/Auth.cs
@@ -2,8 +2,22 @@ using System.Text.Json.Serialization;
 
 namespace StoatApplication.Core.Api.Models;
 
-public record Auth
+public abstract record Auth
 {
+    /// <summary>
+    ///     Response from <c>/auth/session/login</c>
+    /// </summary>
+    /// <param name="Result">Not Documented</param>
+    /// <param name="Id">Unique Id</param>
+    /// <param name="UserId">User Id</param>
+    /// <param name="Token">Session token</param>
+    /// <param name="Name">Display Name</param>
+    /// <param name="LastSeen">When the session was last logged in (iso8601 timestamp)</param>
+    /// <param name="Origin">
+    ///     What is the session origin? This could be used to differentiate sessions that come from
+    ///     staging/test vs prod, etc. Authifier will set this to None by default. The application must fill it in.
+    /// </param>
+    /// <param name="Subscription">Web Push Subcription</param>
     public sealed record LoginResponse(
         [property: JsonPropertyName("result")] string Result,
         [property: JsonPropertyName("_id")] string Id,
@@ -15,18 +29,38 @@ public record Auth
         string LastSeen,
         [property: JsonPropertyName("origin")] string? Origin,
         [property: JsonPropertyName("subscriptions")]
-        Subcriptions Subcriptions
+        Subscription Subscription
     );
 
-    public sealed record Subcriptions(
+    /// <summary>
+    ///     Web Push Subcription
+    /// </summary>
+    /// <param name="Endpoint">Not Documented</param>
+    /// <param name="P256dh">Not Documented</param>
+    /// <param name="Auth">Not Documented</param>
+    public abstract record Subscription(
         [property: JsonPropertyName("endpoint")]
         string Endpoint,
         [property: JsonPropertyName("p256dh")] string P256dh,
         [property: JsonPropertyName("auth")] string Auth
     );
 
+    /// <summary>
+    ///     Information about a specific session
+    /// </summary>
+    /// <param name="Id">Session Id</param>
+    /// <param name="Name">Session Name</param>
     public sealed record Session(
         [property: JsonPropertyName("_id")] string Id,
         [property: JsonPropertyName("name")] string Name
+    );
+
+    /// <summary>
+    ///     Represents a response indicating whether onboarding is required for a user.
+    /// </summary>
+    /// <param name="NeedOnboarding">Determines if the user requires onboarding.</param>
+    public sealed record OnboardingResponse(
+        [property: JsonPropertyName("onboarding")]
+        bool NeedOnboarding
     );
 }
