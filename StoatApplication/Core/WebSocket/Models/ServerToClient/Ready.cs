6ec44d56db233adefc298954f27c488c3896359e
Bloom Perez <springdotgay@tuta.io>
2025-12-20 23:20:45 -0400

    fix: inconsistent imports and properties.
    Signed-off-by: Bloom Perez <springdotgay@tuta.io>

--- StoatApplication/Core/WebSocket/Models/ServerToClient/Ready.cs
+++ StoatApplication/Core/WebSocket/Models/ServerToClient/Ready.cs
@@ -1,20 +1,23 @@
 using System.Collections.Generic;
-using Newtonsoft.Json;
+using System.Text.Json.Serialization;
 using StoatApplication.Core.Api.Models;
 
 namespace StoatApplication.Core.WebSocket.Models.ServerToClient;
 
 public record Ready(
-    [property: JsonProperty("users")] List<User>? Users,
-    [property: JsonProperty("servers")] List<object>? Servers,
-    [property: JsonProperty("channels")] List<object>? Channels,
-    [property: JsonProperty("members")] List<object>? Members,
-    [property: JsonProperty("emojis")] List<object>? Emojis,
-    [property: JsonProperty("user_settings")]
+    [property: JsonPropertyName("users")] List<User>? Users,
+    [property: JsonPropertyName("servers")]
+    List<object>? Servers,
+    [property: JsonPropertyName("channels")]
+    List<object>? Channels,
+    [property: JsonPropertyName("members")]
+    List<object>? Members,
+    [property: JsonPropertyName("emojis")] List<object>? Emojis,
+    [property: JsonPropertyName("user_settings")]
     object? UserSettings,
-    [property: JsonProperty("channel_unreads")]
+    [property: JsonPropertyName("channel_unreads")]
     List<object>? ChannelUnreads,
-    [property: JsonProperty("policy_changes")]
+    [property: JsonPropertyName("policy_changes")]
     List<object>? PolicyChanges
 ) : IEvent
 {
